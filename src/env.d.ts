4462363e7170259fe851fcb21db1bc5a0416e820
pixel <pixel+github@chrissx.de>
2025-07-05 15:42:44 +0200

    add type information for lyrics' albums.yaml and derived types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,28 @@
 /// <reference types="astro/client" />
+
+declare type _ytrack = {
+    name: string,
+    isrc?: string,
+    length: number,
+    lyrics?: string,
+    artists?: [string, ...string[]],
+};
+
+declare type _yalbum = {
+    name: string,
+    upc?: number,
+    artists: [string, ...string[]],
+    release: string,
+    cover?: string,
+    link?: string,
+    tracks: [_ytrack, ..._ytrack[]],
+};
+
+declare type track = _ytrack & Required<Pick<_ytrack, "artists">>;
+declare type album = Omit<Omit<_yalbum, "tracks">, "release">
+    & { tracks: [track, ...track[]], release: import("dayjs").Dayjs };
+
+declare module "*/albums.yaml" {
+    const value: _yalbum[];
+    export default value;
+}

54e72eed4b341f6e7b0af66dccd4d1d760196b59
pixel <pixel.git@chrissx.de>
2026-08-22 16:18:03 +0200

    lib: add albums and tracks

--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,15 @@
+import dayjs from "dayjs";
+import * as YAML from "js-yaml";
+import yalbums from "./data/albums.yaml?raw";
+
 export function csv(csv: string): [string[], string[][]] {
-        const lines = csv.split(/[\r\n]+/).filter(e => e).map(e => e.split(','));
-        const headers = lines.splice(0, 1)[0];
-        return [headers, lines];
+    const lines = csv.split(/[\r\n]+/).filter(e => e).map(e => e.split(','));
+    const headers = lines.splice(0, 1)[0];
+    return [headers, lines];
 }
+
+// TODO: content config loader
+export const albums: album[] = (YAML.load(yalbums) as _yalbum[]).map(a =>
+    ({ ...a, release: dayjs(a.release), tracks: a.tracks.map<track>(t => ({ ...a, ...t })) } as album));
+
+export const tracks: track[] = albums.flatMap(a => a.tracks);

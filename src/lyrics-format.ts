cd70073a2354dd0b6c8c9b343eb2d2f4175f97e1
Janina <foo1.janina@chrissx.de>
2026-08-23 00:37:31 +0000

    inline format constants

--- src/lyrics-format.ts
+++
@@ -1,9 +0,0 @@
-import type { LyricStyle } from "g2mm";
-
-export const FORMATS = ["genius", "musixmatch", "plain"] as const;
-
-export const DEFAULT_FORMAT: LyricStyle = "genius";
-
-export const FORMAT_EVENT = "lyrics-format-change";
-
-export const STORAGE_KEY = "lyrics-format";

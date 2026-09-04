cd515529fe752170d3007121a7ed2971ea1e6873
Janina <foo1.janina@chrissx.de>
2026-09-04 13:42:21 +0000

    support github markdown alerts

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+import { unified } from '@astrojs/markdown-remark';
+import { remarkAlert } from 'remark-github-blockquote-alert';
 import pagefind from './src/pagefind';
 
 export default defineConfig({
     integrations: [pagefind],
+    markdown: {
+        processor: unified({ remarkPlugins: [remarkAlert] }),
+    },
 });
